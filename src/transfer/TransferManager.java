399c698bdf53074315488168fbf54ebdd063a83d
shahdmoamen <shahdmoamem@gmail.com>
2023-11-11 22:18:44 +0200

    add transfer package structure

---
+++ src/transfer/TransferManager.java
@@ -0,0 +1,8 @@
+package transfer;
+
+public class TransferManager {
+    private Transfer transfer;
+    public void setTransferStrategy(Transfer transfer){};
+    public void performTransfer(double amount){
+    };
+}
